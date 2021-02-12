8fe0272afd8f3f9c6fa402d23b0384878447d4bb
kazumaru18 <k.tobe015@gmail.com>
2021-02-12 12:01:55 +0900

    総合演習

---
+++ shop/purchase_output.php
@@ -0,0 +1,47 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>購入画面</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<?php require 'menu.php'; ?>
+	<?php
+		require 'db_connect.php';
+		//purchaseテーブル最終行　id + 1 を取得
+		$purchese_id = 1;
+		foreach ($pdo->query('select max(id) from purchase') as $row){
+			$purchase_id = $row['max(id)'] + 1;
+		}
+		//
+		$sql = "insert into purchase values(:id, :customer_id)";
+		//
+		$stm = $pdo->prepare($sql);
+		//
+		$stm->bindValue(':id', $purchase_id, PDO::PARAM_INT);
+		$stm->bindValue(':customer_id', $_SESSION['customer']['id'], PDO::PARAM_INT);
+		if ($stm->execute()){
+			//
+			//
+			foreach ($_SESSION['product'] as $product_id => $product){
+				$sql = "insert into purchase_detail values(:purchase_id,:product_id,:count)";
+				//
+				$stm = $pdo->prepare($sql);
+				//
+				$stm->bindValue(':purchase_id', $purchase_id, PDO::PARAM_INT);
+				$stm->bindValue(':product_id', $product_id, PDO::PARAM_INT);
+				$stm->bindValue(':count', $product['count'], PDO::PARAM_INT);
+				//
+				$stm->execute();
+			}
+			unset($_SESSION['product']);
+			echo '購入手続きが完了しました。';
+		}else{
+			echo '購入手続き中にエラーが発生しました。';
+		}
+	?>
+</body>
+
+</html>
